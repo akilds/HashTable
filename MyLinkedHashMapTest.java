fa3ac159f2f656f01007d945f35df36fd2e63fc0
akilds <akilrainadesigan@gmail.com>
2021-04-13 22:16:05 +0530

    ADDED : Frequency in phrase

---
+++ MyLinkedHashMapTest.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyLinkedHashMapTest {
+
+   //TEST CASE 2
+	@Test
+	public void testFrequency()
+	{
+		String sentence = "Paranoids are not paranoid because they are paranoid but "+
+	                       "because they keep putting themselves deliberately into "+
+				           "paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for(String word : words)
+		{
+			Integer value = myLinkedHashMap.get(word);
+			if(value==null) value = 1;
+			else value = value + 1;
+			myLinkedHashMap.add(word,value);
+		}
+		int frequency = myLinkedHashMap.get("paranoid");
+		System.out.println(myLinkedHashMap);
+		Assert.assertEquals(3, frequency);
+	}
+}
